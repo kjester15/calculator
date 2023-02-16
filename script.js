dfabf44ae2112d6bb7c139796d3339bd9b9791b2
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-16 15:55:14 -0700

    Create variables for all buttons

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
-function add(a, b) {
+const clear = document.getElementById('clear');
+const backspace = document.getElementById('backspace');
+const percentage = document.getElementById('percentage');
+const divide = document.getElementById('divide');
+const seven = document.getElementById('seven');
+const eight = document.getElementById('eight');
+const nine = document.getElementById('nine');
+const add = document.getElementById('add');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const subtract = document.getElementById('subtract');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const multiply = document.getElementById('multiply');
+const zero = document.getElementById('zero');
+const point = document.getElementById('point');
+const equal = document.getElementById('equal');
+
+function addNum(a, b) {
     return a + b;
 };
 
-function subtract(a, b) {
+function subtractNum(a, b) {
     return a - b;
 };
 
-function multiply(a, b) {
+function multiplyNum(a, b) {
     return a * b;
 };
 
-function divide(a, b) {
+function divideNum(a, b) {
     return a / b;
 };
 
 function operate(a, b, operator) {
     switch(operator) {
         case '+':
-            return add(a,b);
+            return addNum(a,b);
         case '-':
-            return subtract(a,b);
+            return subtractNum(a,b);
         case '*':
-            return multiply(a,b);
+            return multiplyNum(a,b);
         case '/':
-            return divide(a,b);
+            return divideNum(a,b);
     }
 }

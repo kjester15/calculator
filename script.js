739a8613f5d95f7cba135438afe4f532fa7b1b81
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-17 12:21:32 -0700

    Complete backspaceDisplay function to allow removing one digit at a time from currentNumber display

--- script.js
+++ script.js
@@ -20,36 +20,33 @@ const equal = document.getElementById('equal');
 const currentNumber = document.getElementById('currentNumber');
 const runningTotal = document.getElementById('runningTotal');
 
+// event listener for all buttons except clear, backspace, and enter
 const buttons = Array.from(document.querySelectorAll('.populate'));
 buttons.forEach(button => button.addEventListener('click', populateDisplay));
 
+// event listeners for clear, backspace, and enter
 clear.addEventListener('click', clearDisplay);
+backspace.addEventListener('click', backspaceDisplay);
 
 
-// clear.addEventListener('click', [insert function]);
-// backspace.addEventListener('click', [insert function]);
-// percentage.addEventListener('click', [insert function]);
-// divide.addEventListener('click', [insert function]);
-// seven.addEventListener('click', [insert function]);
-// eight.addEventListener('click', [insert function]);
-// nine.addEventListener('click', [insert function]);
-// add.addEventListener('click', [insert function]);
-// four.addEventListener('click', [insert function]);
-// five.addEventListener('click', [insert function]);
-// six.addEventListener('click', [insert function]);
-// subtract.addEventListener('click', [insert function]);
-// one.addEventListener('click', [insert function]);
-// two.addEventListener('click', [insert function]);
-// three.addEventListener('click', [insert function]);
-// multiply.addEventListener('click', [insert function]);
-// zero.addEventListener('click', [insert function]);
-// point.addEventListener('click', [insert function]);
-// equal.addEventListener('click', [insert function]);
+function backspaceDisplay() {
+    if (currentNumber.innerHTML.length == 1) {
+        let currentArray = Array.from(currentNumber.innerHTML);
+        currentArray.pop();
+        currentNumber.innerHTML = '0';
+    }
+    else {
+        let currentArray = Array.from(currentNumber.innerHTML);
+        currentArray.pop();
+        let currentString = currentArray.join('');
+        currentNumber.innerHTML = currentString;
+    }
+};
 
 function clearDisplay() {
     currentNumber.innerHTML = '0';
     runningTotal.innerHTML = '';
-}
+};
 
 function checkDisplay(value) {
     const regex = /[1234567890,]/;
@@ -59,7 +56,7 @@ function checkDisplay(value) {
     else {
         return false;
     }
-}
+};
 
 function populateDisplay() {
     if (currentNumber.innerHTML == '0') {
@@ -71,7 +68,7 @@ function populateDisplay() {
     else {
         currentNumber.innerHTML = this.innerHTML;
     }
-}
+};
 
 function addNum(a, b) {
     return a + b;

81add577366e6c2b0284191f9e8a17b2c208bf49
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-16 17:00:07 -0700

    Add prelim function to update display with clicked number, and update button hover colors

--- script.js
+++ script.js
@@ -18,25 +18,32 @@ const zero = document.getElementById('zero');
 const point = document.getElementById('point');
 const equal = document.getElementById('equal');
 
-clear.addEventListener('click', [insert function]);
-backspace.addEventListener('click', [insert function]);
-percentage.addEventListener('click', [insert function]);
-divide.addEventListener('click', [insert function]);
-seven.addEventListener('click', [insert function]);
-eight.addEventListener('click', [insert function]);
-nine.addEventListener('click', [insert function]);
-add.addEventListener('click', [insert function]);
-four.addEventListener('click', [insert function]);
-five.addEventListener('click', [insert function]);
-six.addEventListener('click', [insert function]);
-subtract.addEventListener('click', [insert function]);
-one.addEventListener('click', [insert function]);
-two.addEventListener('click', [insert function]);
-three.addEventListener('click', [insert function]);
-multiply.addEventListener('click', [insert function]);
-zero.addEventListener('click', [insert function]);
-point.addEventListener('click', [insert function]);
-equal.addEventListener('click', [insert function]);
+const buttons = Array.from(document.querySelectorAll('.button'));
+buttons.forEach(button => button.addEventListener('click', populateDisplay));
+
+// clear.addEventListener('click', [insert function]);
+// backspace.addEventListener('click', [insert function]);
+// percentage.addEventListener('click', [insert function]);
+// divide.addEventListener('click', [insert function]);
+// seven.addEventListener('click', [insert function]);
+// eight.addEventListener('click', [insert function]);
+// nine.addEventListener('click', [insert function]);
+// add.addEventListener('click', [insert function]);
+// four.addEventListener('click', [insert function]);
+// five.addEventListener('click', [insert function]);
+// six.addEventListener('click', [insert function]);
+// subtract.addEventListener('click', [insert function]);
+// one.addEventListener('click', [insert function]);
+// two.addEventListener('click', [insert function]);
+// three.addEventListener('click', [insert function]);
+// multiply.addEventListener('click', [insert function]);
+// zero.addEventListener('click', [insert function]);
+// point.addEventListener('click', [insert function]);
+// equal.addEventListener('click', [insert function]);
+
+function populateDisplay() {
+    document.querySelector('.current-number').innerHTML = this.value;
+}
 
 function addNum(a, b) {
     return a + b;

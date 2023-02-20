7b4c21fff8271c0b4439bafe54b1197ac8ed733f
Kyle Jester <95105911+kjester15@users.noreply.github.com>
2023-02-20 14:52:02 -0700

    Move display answer functionality to it's own function

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ let number2 = 0;
 let answer = 0;
 let currentOperator = '';
 let lastClicked = '';
+let continueCalc = false;
 
 // event listener for all buttons except clear, backspace, percentage and equal
 const populateButtons = Array.from(document.querySelectorAll('.populate'));
@@ -45,6 +46,20 @@ percentage.addEventListener('click', createPercentage);
 const buttons = Array.from(document.querySelectorAll('.button'));
 buttons.forEach(button => button.addEventListener('click', setLastClicked));
 
+// event listener for equal to set continueCalc to true
+equal.addEventListener('click', function () {
+    continueCalc = true;
+});
+
+function displayAnswer (e) {
+    // displays answer in currentNumber when equal is clicked
+    if (e.innerHTML == '=') {
+        currentNumber.innerHTML = answer;
+    }
+    else {
+        currentNumber.innerHTML = '';
+    }
+}
 
 function createPercentage () {
     let newPercentage = currentNumber.innerHTML / 100;
@@ -57,7 +72,7 @@ function setLastClicked () {
 
 function updateRunningTotal () {
     // don't allow operators to be clicked more than once
-    if (lastClicked == '+' || lastClicked == '-' || lastClicked == '/' || lastClicked == 'x' || lastClicked == '=') {
+    if (lastClicked == '+' || lastClicked == '-' || lastClicked == '/' || lastClicked == 'x') {
         return;
     }
     
@@ -65,38 +80,35 @@ function updateRunningTotal () {
     if (currentNumber == '') {
         return;
     }
-    else {
-        if (runningTotal.innerHTML == '') {
-            number1 = Number(currentNumber.innerHTML);
-        }
-        else {
-            number2 = Number(currentNumber.innerHTML);
-            answer = Math.round((operate(number1, number2, currentOperator) + Number.EPSILON) * 100) / 100;
-            number1 = answer;
-            number2 = 0;
-        }
-        
-        // updates running total with operator or without equal sign depending on which is clicked
-        if (this.innerHTML != '=') {
-            let newRunningTotal = runningTotal.innerHTML + ' ' + currentNumber.innerHTML + ' ' + this.innerHTML;
-            runningTotal.innerHTML = newRunningTotal;
-        }
-        else if (this.innerHTML == '=') {
-            let newRunningTotal = runningTotal.innerHTML + ' ' + currentNumber.innerHTML + ' ';
-            runningTotal.innerHTML = newRunningTotal;
-        }
 
-        // displays answer in currentNumber when equal is clicked
-        if (this.innerHTML == '=') {
-            currentNumber.innerHTML = answer;
-        }
-        else {
-            currentNumber.innerHTML = '';
-        }
-        
-        // assigns clicked operator to currentOperator variable
-        currentOperator = this.innerHTML;
+    // if (currentOperator == '=') {
+    //     runningTotal.innerHTML = answer;
+    // }
+    
+    if (runningTotal.innerHTML == '') {
+        number1 = Number(currentNumber.innerHTML);
     }
+    else {
+        number2 = Number(currentNumber.innerHTML);
+        answer = Math.round((operate(number1, number2, currentOperator) + Number.EPSILON) * 10000000000) / 10000000000;
+        number1 = answer;
+        number2 = 0;
+    }
+    
+    // updates running total with operator or without equal sign depending on which is clicked
+    if (this.innerHTML != '=') {
+        let newRunningTotal = runningTotal.innerHTML + ' ' + currentNumber.innerHTML + ' ' + this.innerHTML;
+        runningTotal.innerHTML = newRunningTotal;
+    }
+    else if (this.innerHTML == '=') {
+        let newRunningTotal = runningTotal.innerHTML + ' ' + currentNumber.innerHTML + ' ';
+        runningTotal.innerHTML = newRunningTotal;
+    }
+
+    displayAnswer(this);
+    
+    // assigns clicked operator to currentOperator variable
+    currentOperator = this.innerHTML;
 };
 
 function backspaceDisplay() {
@@ -111,6 +123,7 @@ function backspaceDisplay() {
         let currentString = currentArray.join('');
         currentNumber.innerHTML = currentString;
     }
+    continueCalc = false;
 };
 
 function clearDisplay() {
